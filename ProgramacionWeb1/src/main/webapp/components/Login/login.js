9c049c6b1edea959bbe1218fa807ed73ba7295fe
Alvaro Duron <44824832+AlvarDuron@users.noreply.github.com>
2022-09-14 11:11:06 -0500

    Migracion a netbeans

---
+++ ProgramacionWeb1/src/main/webapp/components/Login/login.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#button-registrarse").click(function () {
+        location.href = "../Register/register.html";
+    });
+
+    $("#logo").click(function () {
+        location.href = "../../index.html";
+    });
+});
---
+++ ProgramacionWeb1/src/main/webapp/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#button-inicio").click(function () {
+        location.href = "./components/Login/login.html";
+    });
+
+    $("#button-registrarse").click(function () {
+        location.href = "./components/Register/register.html";
+    });
+});
